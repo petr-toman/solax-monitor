a192c111cba25a0ea36fcc42696e7acba1b9e187
petr.toman <petr.toman@ergo.cz>
2024-03-04 22:05:09 +0100

    temporary

--- web/htdocs/poll.php
+++ web/htdocs/poll.php
@@ -9,13 +9,7 @@ function unsignedToSigned($val) {
     return ($val > 32767) ? $val - 65536 : $val;
 }
 
-// Funkce pro vykreslení progress baru
-function progress_bar($val, $max) {
-    $bar_length = 20;
-    $lc_progress = ($max == 0) ? 0 : ($val * $bar_length / $max);
-    $progress_bar = str_repeat("#", $lc_progress) . str_repeat("_", $bar_length - $lc_progress);
-    return "[$progress_bar]";
-}
+
 //-------------------
   $inverterModeMap = array(
     "Waiting",
@@ -31,7 +25,6 @@ function progress_bar($val, $max) {
     "Standby"
   );
 
-$divLine="------------------------------------------------\n";
 
 // Volání API Solax pomocí cURL
 
@@ -53,7 +46,6 @@ curl_close($ch);
 $solaxData = json_decode($solaxResult);
 
 // Volání API AZ Router pomocí cURL
-
 $AZRouterPowerUrl = getenv("AZRouterPowerUrl");
 
 $ch= curl_init();
@@ -113,74 +105,14 @@ $solax->AZRPowerL1 = $AZRData->input->power[0]->value;
 $solax->AZRPowerL2 = $AZRData->input->power[1]->value; 
 $solax->AZRPowerL3 = $AZRData->input->power[2]->value; 
 $solax->AZRPowerTotal = $solax->AZRPowerL1 + $solax->AZRPowerL2 + $solax->AZRPowerL3;
- 
+
+$solax->loadHomeL1 =  $solax->llph1 - $solax->AZRPowerL1;
+$solax->loadHomeL2 =  $solax->llph2 - $solax->AZRPowerL2;
+$solax->loadHomeL3 =  $solax->llph3 - $solax->AZRPowerL3;
 
 
-//var_export($solax );
-//var_export($result);
-echo "<pre>";
 
-printf( "\n" );
-printf( "$divLine" );
-printf ( "%s          %s \n", $solax->SerNum, date( "D, Y-m-d h:i:s T" ) );
-printf( "$divLine" );
-printf( "\n" );
+require_once ("poll-text.php");
+$solax->formatted = generateText( $solax );
 
-printf ( "--- PANELY -------------------------------------\n");
-printf( "        celkem: %5d W   %s\n" , $solax->totalPower,   progress_bar($solax->totalPower, $solax->totalPeak) );
-printf( "      string 1: %5d W   %s\n" , $solax->pv1Power,     progress_bar($solax->pv1Power, $solax->SolaxString1Peak) );
-printf( "      string 2: %5d W   %s\n" , $solax->pv2Power,     progress_bar($solax->pv1Power, $solax->SolaxString1Peak) );
-printf( "dnes výroba DC: %5.1f kWh\n",  $solax->totalProduction );
-echo "\n";
-
-printf ( "--- BATERIE ------------------------------------\n");
-printf( "                          %3d %%       %5d °C\n",  $solax->batterySoC, $solax->batteryTemp  );
-printf( "        nabití: %5.1f kWh %s\n",  $solax->batteryCap , progress_bar($solax->batterySoC, 100));
-if (  $solax->batteryPower >= 0){
-printf ("      nabíjení: %5d W\n", $solax->batteryPower) ;
-} else {
-printf ("      vybíjení: %5d W\n", $solax->batteryPower) ;    
-}
-printf ("   dnes nabito: %5.1f kWh\n", $solax->totalChargedIn) ;    
-printf ("        vybito: %5.1f kWh\n", $solax->totalChargedOut) ;   
-echo "\n"; 
-
-printf ( "--- STŘÍDAČ [ %'--35s\n", $solax->inverterMode."]");
-printf ( "                                       %5d °C\n", $solax->inverterTemp);
-printf ("         výkon: %5d kWh %s\n", $solax->inverterPower, progress_bar($solax->inverterPower, $solax->SolaxmaxPower) ) ;   
-printf ("            L1: %5d W\n", $solax->llph1) ;   
-printf ("            L2: %5d W\n", $solax->llph2) ;  
-printf ("            L3: %5d W\n", $solax->llph3) ;  
-printf ("dnes výroba AC: %5.1f kWh\n", $solax->totalProductionInclBatt) ;  
-echo "\n"; 
-
-printf ( "--- DISTRIBUČNÍ SÍŤ ----------------------------\n");
-if ($solax->feedInPower < 0 ) {
-printf ("         odběr: %5d W\n", $solax->feedInPower) ;  
-} else {
-printf ("       dodávka: %5d W\n", $solax->feedInPower) ;          
-}
-if ($solax->AZRPowerL1 < 0  ) {
-printf (" (AZ) L1 odběr: %5d W\n", $solax->AZRPowerL1) ;  
-} else {
-printf ("(AZ)L1 dodávka: %5d W\n", $solax->AZRPowerL1) ;          
-}
-if ($solax->AZRPowerL2 < 0  ) {
-printf (" (AZ) L2 odběr: %5d W\n", $solax->AZRPowerL2) ;  
-} else {
-printf ("(AZ)L2 dodávka: %5d W\n", $solax->AZRPowerL2) ;          
-}
-if ($solax->AZRPowerL3 < 0  ) {
-printf (" (AZ) L3 odběr: %5d W\n", $solax->AZRPowerL3) ;  
-} else {
-printf ("(AZ)L3 dodávka: %5d W\n", $solax->AZRPowerL3) ;          
-}
-printf (" (AZ) L1+L2+L3: %5d W\n", $solax->AZRPowerTotal) ;   
-printf (" dnes odebráno: %5.2f kWh\n", $solax->totalGridIn) ;   
-printf ("        dodáno: %5.2f kWh\n", $solax->totalGridOut) ;   
-echo "\n"; 
-
-printf ("--- DOMÁCNOST ----------------------------------\n");
-printf ("aktuální odběr: %5d W\n", $solax->loadHome) ;   
-printf (" dnes spotřeba: %5.2f kWh\n", $solax->totalConsumption) ;   
-printf ("  soběstačnost:   %3d %%  %s\n", $solax->selfSufficiencyRate, progress_bar($solax->selfSufficiencyRate, 100)) ;
+echo json_encode( $solax );
