f80a0bb7d70f445b388dae642e14185e78b22136
petr.toman <petr.toman@ergo.cz>
2024-03-08 21:43:26 +0100

    fake invertor data generator for testing

--- fake-solax/htdocs/index.php
+++ fake-solax/htdocs/index.php
@@ -1,4 +1,20 @@
 <?php
 header("Content-type:application/json");
-$file = file_get_contents('./index.json', true);
+
+
+$counter = file_get_contents("./lastcall" , true);
+
+if( isset( $counter ) ) {
+    $counter  += 1;
+ } else {
+    $counter = 1;
+ }
+if ( $counter  > 9 ){
+    $counter = 1;
+}
+
+file_put_contents("./lastcall" , $counter );
+$filename = "data-0".$counter.".json";
+
+$file = file_get_contents("./$filename" , true);
 echo $file ;
